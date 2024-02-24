658f13a15ff74cf7c012e9d5f63fda79fd99e206
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-02-24 21:40:12 +0530

    fix: folder structure and path with UI change

---
+++ public/js/merger.js
@@ -0,0 +1,1 @@
+import PDFMerger from "pdf-merger-js";
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(3000);
