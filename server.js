a3f2e34f8ba2d2ea4c7b016c5611dc612fc289de
Kiran Mahajan <thekiranmahajan@outlook.com>
2024-03-01 19:45:58 +0530

    feat: integrate multer with custom configuaration + provide 2 pdf files to mergePDF fn

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const path = require("path");
+const multer = require("multer");
+const { mergePDF } = require("./public/js/mergePDF");
 const app = express();
+
+const upload = multer({
+  dest: "uploads/",
+  limits: { fileSize: 5 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    if (!file.mimetype.startsWith("application/pdf")) {
+      return cb(new Error("Only PDF files are allowed!"));
+    }
+    cb(null, true);
+  },
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -8,9 +21,22 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.post("/merge-pdfs", upload.array("pdfs", 2),async(req, res) => {
-  console.log(req.files)
-  
+app.post("/merge-pdfs", upload.array("pdfs", 2), async (req, res) => {
+  try {
+    if (req.files.length !== 2) {
+      throw new Error("please select exactly two PDFs to merge");
+    }
+    const mergedPdfTime = await mergePDF(
+      path.join(__dirname, req.files[0].path),
+      path.join(__dirname, req.files[1].path)
+    );
+    res.redirect(
+      `http://localhost:3000/public/generatedPDFs/${mergedPdfTime}.pdf`
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 app.listen(3000);
